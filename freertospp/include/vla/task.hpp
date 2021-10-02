0e5fdeed764f5663772a024f6bd42b0411a273c1
Victor Laskurain <victorlaskurain@gmail.com>
2021-10-02 17:16:48 +0200

    Refactor task to use unique_ptr for closure/task management.

--- freertospp/include/vla/task.hpp
+++ freertospp/include/vla/task.hpp
@@ -7,18 +7,46 @@
 namespace vla {
 
 class Task {
-    TaskHandle_t freertos_task;
-    void *task_closure           = nullptr;
-    using Deleter                = void (*)(const void *);
-    Deleter task_closure_deleter = nullptr;
+    struct ClosureHandle {
+        void *handle;
+        ClosureHandle(void *h = nullptr) : handle(h) {
+        }
+        bool operator==(ClosureHandle other) const {
+            return handle == other.handle;
+        }
+        bool operator!=(ClosureHandle other) const {
+            return handle != other.handle;
+        }
+    };
+    struct ClosureDeleter {
+        using pointer = ClosureHandle;
+        using Deleter = void (*)(void *);
+        Deleter deleter;
+        ClosureDeleter(Deleter d = nullptr) : deleter(d) {
+        }
+        void operator()(ClosureHandle c) {
+            deleter(c.handle);
+        }
+    };
+    struct TaskHandleDeleter {
+        using pointer = TaskHandle_t;
+        void operator()(TaskHandle_t h) {
+            vTaskDelete(h);
+        }
+    };
+    using ClosureUniquePtr = std::unique_ptr<ClosureHandle, ClosureDeleter>;
+    using TaskUniquePtr    = std::unique_ptr<TaskHandle_t, TaskHandleDeleter>;
+    ClosureUniquePtr task_closure =
+        ClosureUniquePtr(ClosureHandle(), ClosureDeleter());
+    TaskUniquePtr freertos_task = TaskUniquePtr();
 
   public:
     Task(TaskFunction_t f, const char *name, configSTACK_DEPTH_TYPE stack = 128,
          UBaseType_t priority = tskIDLE_PRIORITY) {
-        auto err =
-            xTaskCreate(f, name, stack, nullptr, priority, &freertos_task);
+        TaskHandle_t task;
+        auto err = xTaskCreate(f, name, stack, nullptr, priority, &task);
         if (err == pdPASS) {
-            task_closure_deleter = [](const void *) {};
+            freertos_task = TaskUniquePtr(task);
         }
     }
 
@@ -26,49 +54,28 @@ class Task {
     Task(const Callable &f, const char *name,
          configSTACK_DEPTH_TYPE stack = 128,
          UBaseType_t priority         = tskIDLE_PRIORITY) {
+        TaskHandle_t task;
         auto tc       = std::make_unique<Callable>(f);
         auto taskCode = [](void *f) { (*static_cast<Callable *>(f))(); };
-        auto err      = xTaskCreate(taskCode, name, stack, tc.get(), priority,
-                               &freertos_task);
+        auto err =
+            xTaskCreate(taskCode, name, stack, tc.get(), priority, &task);
         if (err != pdPASS) {
             return;
         }
-        task_closure         = tc.release();
-        task_closure_deleter = [](const void *ptr) {
-            delete static_cast<const Callable *>(ptr);
-        };
-    }
-
-    /**
-     * Once the destructor is called the task closure data will not be
-     * available, so make sure the task is not going to try such an access
-     * before freeing the instance.
-     */
-    ~Task() {
-        if (task_closure) {
-            vTaskDelete(freertos_task);
-            task_closure_deleter(task_closure);
-        }
+        task_closure =
+            ClosureUniquePtr(tc.release(), ClosureDeleter([](void *ptr) {
+                                 delete static_cast<const Callable *>(ptr);
+                             }));
+        freertos_task = TaskUniquePtr(task);
     }
 
     Task(const Task &) = delete;
-
-    Task(Task &&other) noexcept {
-        freertos_task = std::exchange(other.freertos_task, TaskHandle_t());
-        task_closure  = std::exchange(other.task_closure, nullptr);
-        task_closure_deleter =
-            std::exchange(other.task_closure_deleter, nullptr);
-    }
-
+    Task(Task &&other) = default;
     Task &operator=(const Task &) = delete;
-
-    Task &operator=(Task &&other) {
-        std::swap(*this, other);
-        return *this;
-    }
+    Task &operator=(Task &&other) = default;
 
     operator bool() const {
-        return task_closure_deleter;
+        return !!task_closure;
     }
 };
 
